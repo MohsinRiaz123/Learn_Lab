a88572940e60bb0360dbe934d7bdbdae1800e06b
Mohsin Riaz <mohsin.riaz338@gmail.com>
2025-06-23 01:58:52 +0500

    add vercel file , update index.js

---
+++ Learn Lab/server/dev.js
@@ -0,0 +1,10 @@
+const app = require('./index');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
